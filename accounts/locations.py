18c808586105ddccfe54b11ec590fa61944ad38f
Jeffrey Omamoke <101363689+Neon-jeff@users.noreply.github.com>
2024-08-23 14:49:55 +0100

    feat:added missing country

--- accounts/locations.py
+++ accounts/locations.py
@@ -9,6 +9,19 @@ def CountryData():
         'flag':f'https://flagcdn.com/16x12/{country["iso2"].lower()}.png'
     } for country in countries.all_countries().states_file if len(country['states'])!=0
       ]
+    data.append({
+        "name":"Hong Kong",
+        "phone_code":852,
+        "country_code":"HK",
+        "states":[
+    "Central and Western", "Eastern", "Southern", "Wan Chai",
+    "Sham Shui Po", "Kowloon City", "Kwun Tong", "Wong Tai Sin", "Yau Tsim Mong",
+    "Islands", "Kwai Tsing", "North", "Sai Kung", "Sha Tin", "Tai Po", "Tsuen Wan", "Tuen Mun", "Yuen Long"
+],
+"flag":"https://flagcdn.com/16x12/hk.png"
+    })
+    data=sorted(data,key=lambda k : k['name'])
+    
     
     return data
 
