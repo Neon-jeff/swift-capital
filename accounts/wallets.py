ff793ee9c62b59ae5406a9026db729bf5288c818
Neon-jeff <omaksteejay@gmail.com>
2025-04-05 14:13:34 +0100

    feat:wallet configs

--- accounts/wallets.py
+++ accounts/wallets.py
@@ -1,103 +1,101 @@
-wallet_address=[
-        {
-            "name":"BTC",
-            "address":"bc1qzrfrgl3724mperf2yfj6x6lz46vq4ueuekrh8h",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
-        },
-                {
-            "name":"ETH",
-            "address":"0x1d6A91643e8eC808a631eA407549E47d1A8A95b2",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
-        },
-                {
-            "name":"USDC",
-            "address":"0x1d6A91643e8eC808a631eA407549E47d1A8A95b2",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"
-        },
-                {
-            "name":"SOL",
-            "address":"7UCrfvnQueCAv8CvEgLAR9D2hLUK73N8piRoTUZupRJf",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png"
-        },
-                {
-            "name":"XRP",
-            "address":"r4sB7FUqpPpQcMKDzKwWVabANJetfCoefZ",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/52.png"
-        },
-                {
-            "name":"ADA",
-            "address":"addr1q9v0ewrg6pw6rflngn7y6alu98tkreqg52tuypdcjqp405zcljux35za5xnlx38uf4mlc2whv8jq3g5hcgzm3yqr2lgqcw9nay",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png"
-        },
-                {
-            "name":"XLM",
-            "address":"GAGPB22G7V7QZMDPTOK4JG256QU5QJTEOPBUGOZNPBKFTHGR3WGD6PR4",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/512.png"
-        },
-                {
-            "name":"BNB",
-            "address":"bnb1vp0xpxj00u0msy4r35dj3xv0ggmk8g7m6ujk6k",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png"
-        },
-                {
-            "name":"DOGE",
-            "address":"DLXjjitFEXdW7e2w9wFFNMdPMccPrQyY7y",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/74.png"
-        }
-    ]
+wallet_address = [
+    {
+        "name": "BTC",
+        "address": "bc1qzrfrgl3724mperf2yfj6x6lz46vq4ueuekrh8h",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
+    },
+    {
+        "name": "ETH",
+        "address": "0x1d6A91643e8eC808a631eA407549E47d1A8A95b2",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+    },
+    {
+        "name": "USDC",
+        "address": "0x1d6A91643e8eC808a631eA407549E47d1A8A95b2",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+    },
+    {
+        "name": "SOL",
+        "address": "7UCrfvnQueCAv8CvEgLAR9D2hLUK73N8piRoTUZupRJf",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
+    },
+    {
+        "name": "XRP",
+        "address": "r4sB7FUqpPpQcMKDzKwWVabANJetfCoefZ",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png",
+    },
+    {
+        "name": "ADA",
+        "address": "addr1q9v0ewrg6pw6rflngn7y6alu98tkreqg52tuypdcjqp405zcljux35za5xnlx38uf4mlc2whv8jq3g5hcgzm3yqr2lgqcw9nay",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png",
+    },
+    {
+        "name": "XLM",
+        "address": "GAGPB22G7V7QZMDPTOK4JG256QU5QJTEOPBUGOZNPBKFTHGR3WGD6PR4",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/512.png",
+    },
+    {
+        "name": "BNB",
+        "address": "bnb1vp0xpxj00u0msy4r35dj3xv0ggmk8g7m6ujk6k",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png",
+    },
+    {
+        "name": "DOGE",
+        "address": "DLXjjitFEXdW7e2w9wFFNMdPMccPrQyY7y",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/74.png",
+    },
+]
 
 
-deposit_address=[
-        {
-            "name":"BTC",
-            "address":"bc1qp00d8xl6394nlgvvzc9sc4ymn9kud2va8k9etk",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
-        },
-        #         {
-        #     "name":"ETH",
-        #     "address":"0xF57DBF1F9323283AA6031dA8B07284CE2ab32b0D",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
-        # },
-        #          {
-        #     "name":"XRP",
-        #     "address":"r4A9LSN2K7xKeybJkK6PBjPpHq1RCCtbwb",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/52.png"
-        # },
-                        {
-            "name":"USDT(TRC-20)",
-            "address":"TNpr32fvCCtrCo3yPCsLc2rwM3h5YowJwe",
-            "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/825.png"
-        },
-        # {
-        #     "name":"SOL",
-        #     "address":"9xB51EBjDpLYubENmYwTvCmrdPPhhE7AwKV4KnvySrt9",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png"
-        # },
-        #    {
-        #     "name":"XLM",
-        #     "address":"GBT563XUXBSSZ5PQSGP7V7UTTUT6MTKKXBS2JENYAYPQKCNQU4YOL2EL",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/512.png"
-        # },
-        #  {
-        #     "name":"SHIB",
-        #     "address":"0xF57DBF1F9323283AA6031dA8B07284CE2ab32b0D",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png"
-        # },
-        #          {
-        #     "name":"DOT",
-        #     "address":"16u7c1inJLkHe58u1ZtpvF1pTSAXAtbHPFQnz2U2iSMaF5Z",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png"
-        # },
-        # {
-        #     "name":"DOGE",
-        #     "address":"DCwQ2UJtQHCpiyWxSeQs6JchnM8n6BYXkr",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/74.png"
-        # },
-        #                 {
-        #     "name":"ETH (CHAINLINK)",
-        #     "address":"0xF57DBF1F9323283AA6031dA8B07284CE2ab32b0D",
-        #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
-        # },
-    ]
-
-
+deposit_address = [
+    {
+        "name": "BTC",
+        "address": "bc1qpsuwmrfjzkr306e38lyfcqm9rxnyccchyey06f",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
+    },
+    {
+        "name": "ETH",
+        "address": "bc1qpsuwmrfjzkr306e38lyfcqm9rxnyccchyey06f",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+    },
+    {
+        "name": "USDT(ERC-20)",
+        "address": "0x0e3F1B5B4bd5199CaBE095E6e4cbCa226fD6FB4B",
+        "image": "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png",
+    },
+    {
+        "name":"XRP",
+        "address":"rDrmpbUdaMurVm4bt7nwN4XmjG45M7edqZ",
+        "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/52.png"
+    },
+    {
+        "name":"SOL",
+        "address":"rDrmpbUdaMurVm4bt7nwN4XmjG45M7edqZ",
+        "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png"
+    },
+    #    {
+    #     "name":"XLM",
+    #     "address":"GBT563XUXBSSZ5PQSGP7V7UTTUT6MTKKXBS2JENYAYPQKCNQU4YOL2EL",
+    #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/512.png"
+    # },
+    #  {
+    #     "name":"SHIB",
+    #     "address":"0xF57DBF1F9323283AA6031dA8B07284CE2ab32b0D",
+    #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png"
+    # },
+    #          {
+    #     "name":"DOT",
+    #     "address":"16u7c1inJLkHe58u1ZtpvF1pTSAXAtbHPFQnz2U2iSMaF5Z",
+    #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png"
+    # },
+    {
+        "name":"DOGE",
+        "address":"D5xGrVc9bC9pFubi9xZmaxkcuu3jPQSs2N",
+        "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/74.png"
+    },
+    #                 {
+    #     "name":"ETH (CHAINLINK)",
+    #     "address":"0xF57DBF1F9323283AA6031dA8B07284CE2ab32b0D",
+    #     "image":"https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
+    # },
+]
