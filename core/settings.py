cea86f4b2e1f2cb4f61d4e61ddcfcb1833a23171
Neon-jeff <omaksteejay@gmail.com>
2025-04-05 06:10:39 +0100

    feat:super user

--- core/settings.py
+++ core/settings.py
@@ -181,9 +181,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # COMPRESSOR CONFIGS
 
-CSRF_TRUSTED_ORIGINS=[
-   "https://www.apex-signals.com"
-]
+CSRF_TRUSTED_ORIGINS = ["https://www.apexfinance.live"]
 
 # UNFOLD CONFIGS
 UNFOLD = {
